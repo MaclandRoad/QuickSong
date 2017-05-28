b1f2099d1e7ad606a2d8a3c29bbd6f78a4a9b79a
judge2020 <hunterray55@hotmail.com>
2017-05-28 13:48:48 -0400

    FrmIndexes

--- QuickSong/FrmMain.Designer.cs
+++ QuickSong/FrmMain.Designer.cs
@@ -28,21 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.BtnIndexes = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BtnIndexes});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(400, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // BtnIndexes
+            // 
+            this.BtnIndexes.Name = "BtnIndexes";
+            this.BtnIndexes.Size = new System.Drawing.Size(46, 22);
+            this.BtnIndexes.Text = "Indexes";
+            this.BtnIndexes.Click += new System.EventHandler(this.BtnIndexes_Click);
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(400, 162);
+            this.Controls.Add(this.toolStrip1);
             this.Name = "FrmMain";
             this.Text = "QuickSong";
             this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel BtnIndexes;
     }
 }
 
