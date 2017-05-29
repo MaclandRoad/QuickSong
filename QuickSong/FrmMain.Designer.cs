f52b9d1399b0597415bea1f3ced99dcae49124e1
MaclandRoadOnPremise <37507887+MaclandRoadOnPremise@users.noreply.github.com>
2017-05-29 08:38:10 -0400

    version box

--- QuickSong/FrmMain.Designer.cs
+++ QuickSong/FrmMain.Designer.cs
@@ -30,9 +30,10 @@
         {
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.BtnIndexes = new System.Windows.Forms.ToolStripLabel();
-            this.BtnSettings = new System.Windows.Forms.ToolStripLabel();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.BtnSettings = new System.Windows.Forms.ToolStripLabel();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,16 +56,16 @@
             this.BtnIndexes.Text = "Indexes";
             this.BtnIndexes.Click += new System.EventHandler(this.BtnIndexes_Click);
             // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
             // BtnSettings
             // 
             this.BtnSettings.Name = "BtnSettings";
             this.BtnSettings.Size = new System.Drawing.Size(49, 22);
             this.BtnSettings.Text = "Settings";
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
             // 
             // textBox1
             // 
@@ -75,11 +76,22 @@
             this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(465, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(532, 149);
+            this.ClientSize = new System.Drawing.Size(532, 81);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.toolStrip1);
             this.Name = "FrmMain";
@@ -99,6 +111,7 @@
         private System.Windows.Forms.ToolStripLabel BtnSettings;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
